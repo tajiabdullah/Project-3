eb946eb9b9cf736acace9f8aa325aa035836a2d2
Taji Abdullah <tajiabdullah@Tajis-MacBook-Pro.local>
2022-04-23 13:11:02 -0700

    Updated file to remove index from JSON

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
+##################################################
+# Dependencies
+##################################################
+
 from flask import Flask, render_template
 import pandas as pd
 from sqlalchemy import create_engine
+
+##################################################
+# Flask Initiation
+##################################################
+
 app = Flask(__name__)
 
+##################################################
+# Flask Routes
+##################################################
 
-# create my own API from the data stored in Postgres database
+# Created API from the data stored in Postgres database
 @app.route('/world_happiness')
-def hello():
+def happiness():
     engine = create_engine('postgresql://postgres:postgres@localhost:5432/world_happiness')
     df = pd.read_sql_table("world_happiness_data",con=engine)
-    return df.to_json(orient='index', indent=4)
-# load the index.html when requesting the https://localhost:5000
+    return df.to_json(orient='split', index=False, indent=4)
+
+# Loaded the index.html when requesting the https://localhost:5000
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+##################################################
+# Close Out by Defining Main Behavior
+##################################################
 
 if __name__ == '__main__':
     app.run(debug=True)
